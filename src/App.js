8cfa40f99465a91724901f220f8b0a5c226b9c89
Riley Bergamasco <rlbergamasco@gmail.com>
2021-11-23 22:30:03 -0500

    Added basic map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 35.90871,
+          lng: -79.063032,
+        }}
+      />
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_API_KEY,
+})(MapContainer);
