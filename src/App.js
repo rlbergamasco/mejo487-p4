f0111c00f65601e1feb9900a8dd007b23adbc876
Riley Bergamasco <rlbergamasco@gmail.com>
2021-11-30 14:37:51 -0500

    added responsive zoom and castle icons

--- src/App.js
+++ src/App.js
@@ -22,6 +22,15 @@ const App = () => {
   const [openDialog, setOpenDialog] = useState(false);
   const [openInfo, setOpenInfo] = useState(false);
   const [openSidebar, setOpenSidebar] = useState(false);
+  const zoom = () => {
+    if (window.innerWidth < 400) {
+      return 3;
+    } else if (window.innerWidth < 1250) {
+      return 4;
+    } else {
+      return 5;
+    }
+  }
 
   axios.get('data.json')
     .then(res => setLocations(res.data))
@@ -33,7 +42,7 @@ const App = () => {
     width: "100%",
   };
 
-  const defaultCenter = { lat: 40.69742713625295, lng: -99.3309117900806 };
+  const defaultCenter = { lat: 39.936735262817066, lng: -97.68019476129098 };
 
   const [center, setCenter] = useState(defaultCenter);
 
@@ -101,7 +110,7 @@ const App = () => {
         <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
           <GoogleMap
             mapContainerStyle={mapStyles}
-            zoom={4}
+            zoom={zoom()}
             center={center}
           >
             {locations.map((item) => {
@@ -111,7 +120,7 @@ const App = () => {
                   name={item.name}
                   position={item.location}
                   onClick={() => handleMarkerClick(item)}
-                // icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
+                  icon="images/castle-icon.png"
                 />
               );
             })}
